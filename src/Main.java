c38b845badc2b101727b8c96cd8fa95628bb1ed0
Garrett <emrickgarrett@gmail.com>
2014-12-17 16:17:47 -0500

    oops, i dumb. Init

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Garrett
+ *
+ * FIP is just a little Image Processing Project I was wanting to work on
+ * Going to make a little tool that can make adjustments to pictures,
+ * create pictures, and basic image editing tools. FIP (Free Image Processing)
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
